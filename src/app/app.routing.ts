2923b37c90985ea06605bac25574d93d8cb064b0
Fernando López <fernando@devanddel.com>
2022-03-07 10:13:39 +0100

    Added product component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 
 const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'product/:id', component: ProductDetailComponent },
 ];
 
 // configures NgModule imports and exports
