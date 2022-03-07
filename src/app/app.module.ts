7b7d2c62b7c8e816bca057332d8ed71a18b842d5
Fernando López <fernando@devanddel.com>
2022-03-07 08:51:46 +0100

    Rebase master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+=======
+>>>>>>> d5fb5b6 (initial commit)
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+<<<<<<< HEAD
     BrowserModule,
     BrowserAnimationsModule
+=======
+    BrowserModule
+>>>>>>> d5fb5b6 (initial commit)
   ],
   providers: [],
   bootstrap: [AppComponent]
