ecffa552253b8c4aa6439507da5be6287a3ff074
Osman Mehmed <osman@devanddel.com>
2022-03-09 10:34:34 +0100

    Actualizacion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,10 +29,14 @@ export class LoginComponent implements OnInit {
     let correo: string = "osman@devanddel.com";
 
     this.formLogin = this.formulario.group({
-/*    email: ['', [Validators.required, Validators.email]],
-      contrasena: ['', [Validators.required, Validators.minLength(5)]], */
-      email: new FormControl(''),
-      contrasena: new FormControl('')
+
+      email: new FormControl('', [  Validators.required,
+                                    Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+                                  ]),
+
+      contrasena: new FormControl('', [ Validators.required,
+                                        Validators.minLength(8),
+      ])
     })
 
     console.log(this.clientes[3].getCorreo());
