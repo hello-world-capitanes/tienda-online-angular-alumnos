7a62851f5f7e9c753fded85403ec108291c02615
Mario Querol <marioquerolamondarain@gmail.com>
2022-03-28 11:02:13 +0200

    Se genera estructura HabitualesComponent

---
+++ src/app/features/habituales/habituales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habituales',
+  templateUrl: './habituales.component.html',
+  styleUrls: ['./habituales.component.scss']
+})
+export class HabitualesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
