588eff02da6060426d0be8dc86676f8c3fbb75c5
raulpradanas <raulpradanas@devanddel.com>
2022-04-21 12:16:56 +0200

    added cypress

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   apiUrl: "https://tienda-online-server.herokuapp.com/api/v1",
   firebase: {
-    projectId: 'tienda-online-mercadona',
+    projectId: 'tienda-online-raul-pradanas',
     appId: '1:906480262910:web:1531bd84403406bedb4faf',
-    storageBucket: 'tienda-online-mercadona.appspot.com',
+    storageBucket: 'tienda-online-raul-pradanas.appspot.com',
     apiKey: 'AIzaSyCRqp8yLl7lqDbO3TjtjJC-m9XQjUB2ktk',
-    authDomain: 'tienda-online-mercadona.firebaseapp.com',
+    authDomain: 'tienda-online-raul-pradanas.firebaseapp.com',
     messagingSenderId: '906480262910',
     measurementId: 'G-LM1E2KEBFY',
   }
